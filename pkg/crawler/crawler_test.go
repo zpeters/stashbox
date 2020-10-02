905482a1159ec128876384e141ad5e6a6c3f2504
Lucas Turci <lucas.turci@gmail.com>
2020-10-02 11:34:25 -0300

    Create test for getHtmlTitle function

--- pkg/crawler/crawler_test.go
+++ pkg/crawler/crawler_test.go
@@ -1,7 +1,26 @@
 package crawler
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
-func TestDummy(t* testing.T) {
+func TestDummy(t *testing.T) {
 
+}
+
+func TestGetHtmlTitle(t *testing.T) {
+	const url = "https://github.com/zpeters/stashbox"
+	const want = "GitHub - zpeters/stashbox: Your personal Internet Archive"
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	title, err := getHtmlTitle(resp)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if title != want {
+		t.Errorf("Wrong title found. Want: %s, Got : %s", want, title)
+	}
 }
