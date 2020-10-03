aa46998079d9da62bd79f308f0ba90846fa5f62a
Shivamani Patil <shivamanipatil10@gmail.com>
2020-10-03 18:13:17 +0530

    Added goroutine support and multiple urls input support

--- cmd/stashbox/main.go
+++ cmd/stashbox/main.go
@@ -1,28 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
 	"stashbox/pkg/archive"
 	"stashbox/pkg/crawler"
+	"sync"
 )
 
-var u string
+var crawl bool
 var basePath string
 var listFlag bool
 
 func main() {
 	// get flags
-	flag.StringVar(&u, "url", "", "url to download")
+	flag.BoolVar(&crawl, "crawl", false, "crawl and save websites")
 	flag.StringVar(&basePath, "b", "./stashDb", "folder to save stash into")
 	flag.BoolVar(&listFlag, "list", false, "list saved archives")
 	flag.Parse()
-
-	if len(os.Args) == 1 {
-		flag.Usage()
-		os.Exit(0)
-	}
 
 	if listFlag {
 		archives, err := archive.GetArchives(basePath)
@@ -40,21 +37,58 @@ func main() {
 		os.Exit(0)
 	}
 
-	if u != "" {
-		c, err := crawler.NewCrawler(basePath)
-		if err != nil {
-			panic(err)
+	if crawl {
+		var n int
+		var urls []string
+		scanner := bufio.NewScanner(os.Stdin)
+
+		// inputs
+		fmt.Println("Enter number of urls: ")
+		fmt.Scan(&n)
+		fmt.Println("Enter the urls: ")
+		for i := 0; i < n; i++ {
+			scanner.Scan()
+			text := scanner.Text()
+			if len(text) != 0 {
+				urls = append(urls, text)
+			} else {
+				break
+			}
 		}
 
-		c.AddUrl(u)
-		err = c.Crawl()
-		if err != nil {
-			panic(err)
+		// spin goroutine for each url 
+		var wg sync.WaitGroup
+		wg.Add(len(urls))
+		for _, url := range urls {
+			go crawlUtil(url, &wg)
 		}
+		wg.Wait()
+	} else {
+		flag.Usage()
+		os.Exit(0)
 
-		err = c.Save()
-		if err != nil {
-			panic(err)
-		}
+	}
+}
+
+func crawlUtil(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	c, err := crawler.NewCrawler(basePath)
+	if err != nil {
+		panic(err)
+	}
+
+	c.AddUrl(url)
+	err = c.Crawl()
+	if err != nil {
+		fmt.Printf("\nError : %s\n", url)
+		fmt.Print(err)
+		return
+	}
+
+	err = c.Save()
+	if err != nil {
+		fmt.Printf("\nError : %s\n", url)
+		fmt.Print(err)
+		return
 	}
 }
