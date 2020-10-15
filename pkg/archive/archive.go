2757641e0f4a59fbf9828839dcd79a5cdaecd831
erizkiatama <erizkiatama@gmail.com>
2020-10-15 15:47:22 +0800

    Fixed list command error when building the archives

--- pkg/archive/archive.go
+++ pkg/archive/archive.go
@@ -2,7 +2,9 @@ package archive
 
 import (
 	"fmt"
+	"sort"
 	"strings"
+	"time"
 
 	"github.com/bmatcuk/doublestar/v2"
 )
@@ -32,26 +34,49 @@ func GetArchives(basePath string) (archives []Archive, err error) {
 }
 
 func buildArchives(path string, files []string) ([]Archive, error) {
-	var archives = []Archive{}
+	var archive *Archive
+	var dates []string
 	var pPage string
 	var page string
-	dates := make([]string, 0)
+
+	archives := []Archive{}
+	setOfArchives := make(map[*Archive]bool)
 	path += "/"
+
 	for _, file := range files {
 		pieces := strings.Split(strings.TrimPrefix(file, strings.TrimLeft(path, "./")), "/")
 		page = strings.Join(pieces[0:len(pieces)-1], "/")
 		date := strings.TrimRight(pieces[len(pieces)-1], ".pdf")
 
-		if page != pPage && pPage != "" {
+		if _, err := time.Parse("2006-02-01T15:04:05", date); err != nil {
 			return archives, fmt.Errorf("Error building archive")
+		}
+		if page != pPage {
+			dates = make([]string, 0)
+			archive = &Archive{URL: page}
 		}
 
 		dates = append(dates, date)
+		archive.Dates = dates
+
+		if _, present := setOfArchives[archive]; !present {
+			setOfArchives[archive] = true
+		}
+
 		pPage = page
+
 	}
 
-	a := Archive{page, dates}
-	archives = append(archives, a)
+	for key := range setOfArchives {
+		archives = append(archives, *key)
+	}
+
+	// Sort the archives because it is not sorted when using a map.
+	// Important for "open" command to make sure it is in the same
+	// order every time getArchives is called
+	sort.Slice(archives, func(i, j int) bool {
+		return archives[i].URL < archives[j].URL
+	})
 
 	return archives, nil
 }
